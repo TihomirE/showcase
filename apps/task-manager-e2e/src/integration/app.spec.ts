fc2ad4e23ac6eea2e4a04e3e63b4ff7f9306e076
TihomirE <elijas.tihomir@gmail.com>
2021-10-20 16:22:14 +0200

    loginpage unit test updated

--- apps/task-manager-e2e/src/integration/app.spec.ts
+++ apps/task-manager-e2e/src/integration/app.spec.ts
@@ -1,13 +1,21 @@
-import { getGreeting } from '../support/app.po';
+import { getTitle } from '../support/app.po';
 
 describe('task-manager', () => {
   beforeEach(() => cy.visit('/'));
 
   it('should display welcome message', () => {
     // Custom command example, see `../support/commands.ts` file
-    cy.login('my-email@something.com', 'myPassword');
+    // cy.login('my-email@something.com', 'myPassword');
 
     // Function helper example, see `../support/app.po.ts` file
-    getGreeting().contains('Welcome to task-manager!');
+    getTitle().contains('Task Manager Login');
+
+    cy.get('p[id="username"]').contains('Username');
+    cy.get('input[name="username"]').should('be.empty');
+
+    cy.get('p[id="password"]').contains('Password');
+    cy.get('input[name="password"]').should('be.empty');
+
+    cy.get('button[id="loginBtn"]').contains('Login');
   });
 });
