14b90deb151ae47eb659af6e8c417e595c81bf64
Tiho <t.elijas@media-management.at>
2021-11-09 19:52:25 +0100

    localization updated

--- libs/feature/authentication/src/lib/feature-auth.module.ts
+++ libs/feature/authentication/src/lib/feature-auth.module.ts
@@ -3,15 +3,15 @@ import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './feature_components/login/login.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { UILoadingModule} from '@showcase/shared/ui-components/loading';
+import { UILoadingModule } from '@showcase/shared/ui-components/loading';
 import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { TranslateModule } from '@ngx-translate/core';
 import { SharedLocalizationModule } from '@showcase/shared/localization';
 
 const routes: Routes = [
-    { path: '', redirectTo: 'login', pathMatch: 'full' },
-    { path: 'login', pathMatch: 'full', component: LoginComponent }
-  ]
+  { path: '', redirectTo: 'login', pathMatch: 'full' },
+  { path: 'login', pathMatch: 'full', component: LoginComponent }
+]
 
 @NgModule({
   imports: [
@@ -28,4 +28,4 @@ const routes: Routes = [
   ],
   schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
-export class FeatureAuthModule {}
+export class FeatureAuthModule { }
